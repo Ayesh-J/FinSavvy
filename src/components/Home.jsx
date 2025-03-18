7832e10352846920abb2bd533c011f1b712240a5
Ayesh-J <jamadarayesh9@gmail.com>
2025-03-18 13:18:23 +0530

    Fix routing issues on GitHub Pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,10 +15,10 @@ const Home = () => {
                     </div>
                     <div className="navbar__items">
                         <ul>
-                            <li><a href="">Home</a></li>
-                            <li><a href="./About">About</a></li>
-                            <li><a href="./Dashboard">Dashboard</a></li>
-                            <li><a href="./Contact">Contact</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li> <Link to="/About">About</Link></li>
+                            <li><Link to="/Dashboard">Dashboard</Link></li>
+                            <li><Link to="/Contact">Contact</Link></li>
                         </ul>
                     </div>
                 </div>
